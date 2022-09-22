bc1f19eb6e0163e94836e5312590e0e9d17670d9
Diego Di Ciurcio <diciurciodiego@gmail.com>
2022-09-22 07:20:54 -0300

    creando click event, solucione tema JS de galeria

--- js/galeria.js
+++ js/galeria.js
@@ -1,8 +1,14 @@
-const imagenes = document.querySelector(".img");
+const imagenes = document.querySelectorAll(".img");
 const containerFullImg = document.querySelector(".containerFullImg");
 
 
+imagenes.forEach(imagen =>{
+imagen.addEventListener ("click", () => {
+    console.log(imagen.getAttribute('alt'));
+    mostrarImgGrande(imagen.getAttribute('alt'), imagen.getAttribute('alt'))
+})
+}); 
 
-imagenes.forEach(imagen => {
-    
-});
+function mostrarImgGrande(src, alt){
+    containerFullImg.classList.toggle("desplazamiento")
+}
